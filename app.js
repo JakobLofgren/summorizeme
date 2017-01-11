7da4653ecbdbe2a43192324fd2af08046b840b78
JakobLofgren <jakob.lofgren1@gmail.com>
2017-01-11 14:28:51 +0100

    Fixed tweets being summorized coming from the bot itself. Whoops

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const Twitter = require("twitter")
 		access_token_key: process.env.access_token_key,
 		access_token_secret: process.env.access_token_secret
 })
-
 winston.level = "debug"
 winston.add(winston.transports.File, { filename: "logs.txt" })
 
@@ -21,6 +20,7 @@ let pics = []
 
 T.stream("user", (stream) => {
 	stream.on("data", (event) => {
+		if(event.user.id_str === "816221605282062336")return
 		winston.debug(event)
 		if(event.entities.urls.length && event.entities.urls.length < 2){
 			summorizePage(event.entities.urls[0].expanded_url)
@@ -50,23 +50,20 @@ T.stream("user", (stream) => {
 		} 
 		else if(event.entities.urls.length && event.entities.urls.length > 1){
 			T.post("statuses/update", {
-				status: `Hey @${event.user.screen_name}, I can only do 1 link per tweet. Sorry 😕 \n \n @summorizeme`,
+				status: `Hey @${event.user.screen_name}, I can only do 1 link per tweet. Sorry 😕 \n @summorizeme`,
 				in_reply_to_status_id: event.id_str
 			})
 			.then(() => T.post("favorites/create", {
 				id: event.id_str
 			}))
 		} else {
-			if(event.user.id_str === "816221605282062336") return 
-			else {
-				T.post("statuses/update", {
-					status: `Hey @${event.user.screen_name}, it seems you broke the rules. Read how it works here [LINK]`,
-					in_reply_to_status_id: event.id_str
-				})
-				.then(() => T.post("favorites/create", {
-					id: event.id_str
-				}))
-			}
+			T.post("statuses/update", {
+				status: `Hey @${event.user.screen_name}, it seem's you're lost 😨. Read how to here: https://t.co/TurI4YTdpJ`,
+				in_reply_to_status_id: event.id_str
+			})
+			.then(() => T.post("favorites/create", {
+				id: event.id_str
+			}))
 		}
 	})
 
